9d7107c7683ee749b2312917b17e46f2666e3073
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-01-01 17:24:46 +0530

    adding filter method to Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,30 @@
 import ResCard from "./ResCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListofRestaurants] = useState(
+    resList.restaurants
+  );
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("clicked");
+            const filteredList = resList.restaurants.filter(
+              (res) => res.info.avgRating > 4.5
+            );
+            setListofRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
 
       <div className="res-container">
-        {resList.restaurants.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <ResCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
