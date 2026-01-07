86be6e2beed7c5dbcf8aa09da85887e890f339c9
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-01-07 16:35:40 +0530

    adding UserClass based components

---
+++ src/components/UserClass.js
@@ -0,0 +1,14 @@
+import React from "react";
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Nitesh</h2>
+        <h3>Location: Mumbai</h3>
+        <h4>Email: nitesh@gmail.com</h4>
+      </div>
+    );
+  }
+}
+
+export default UserClass;
