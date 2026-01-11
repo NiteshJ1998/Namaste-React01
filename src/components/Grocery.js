3e43c520ddb972e2df05093458cffa6cc6f2b2dd
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2026-01-11 15:15:37 +0530

    added Grocery components it has different bundle of js

---
+++ src/components/Grocery.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Grocery = () => {
+  return (
+    <div>
+      <h2>
+        this is Grocery components which has many components inside it has
+        seperate js bundle for that
+      </h2>
+    </div>
+  );
+};
+
+export default Grocery;
